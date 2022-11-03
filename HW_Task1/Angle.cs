b1b452cfcb0350686be6e754457dd147dac3bfa1
Кирилл Зайцев <moja.orbita@ya.ru>
2022-11-04 01:22:22 +0300

    Таск1

---
+++ HW_Task1/Angle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Task1
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool IsCorrect = true;
+
+        public int Gradus
+        {
+            get { return gradus; }
+            set
+            {
+                if (value < 0)
+                    IsCorrect = false;
+                else
+                    gradus = value;
+            }
+        }
+        public int Min
+        {
+            get { return min; }
+            set
+            {
+                if (value < 0)
+                    IsCorrect = false;
+                else
+                    min = value;
+            }
+        }
+        public int Sec
+        {
+
+            get { return sec; }
+            set
+            {
+                if (value < 0)
+                    IsCorrect = false;
+                else
+                    sec = value;
+            }
+        }
+        public Angle(int gradus, int min, int sec) //Конструктор
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+        public double ToRadians()
+        {
+            return ((gradus + min / 60 + sec/60/60) * Math.PI) / 180;
+        }
+    }
+}
---
+++ HW_Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+//Реализовать класс, в котором указанные значения представлены в виде свойств.
+//Для свойств предусмотреть проверку корректности данных.
+//Класс должен содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы.
+//Создать объект на основе разработанного класса. Осуществить использование объекта в программе.
+namespace HW_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool VvodCorrect;
+            do
+            {
+                VvodCorrect = true;
+                try
+                {
+                    Angle angle;
+                    do
+                    {
+                        Console.Write("Градус:  ");
+                        int InputGradus = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Минуты:  ");
+                        int InputMinute = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Секунды: ");
+                        int InputSecond = Convert.ToInt32(Console.ReadLine());
+
+                        angle = new Angle(InputGradus, InputMinute, InputSecond);
+
+
+                    }
+                    while (angle.IsCorrect == false);
+
+                    double TranslatRes = angle.ToRadians();
+                    Console.Write("перевод... ");
+                    Console.WriteLine("{0:f5}", TranslatRes);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    VvodCorrect = false;
+                }
+            }
+            while (VvodCorrect==false);
+            Console.ReadKey();
+
+        }
+    }
+}
